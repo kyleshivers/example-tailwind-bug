846a4c36f33c58d3726939532e908be76937f96e
kyleshivers1 <kyleshivers1@gmail.com>
2024-08-27 11:22:06 -0700

    init commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <div className="flex m-4 h-svh text-slate-700 text-center flex-col">
+      <h1>this is the home page</h1> <div>this is another div</div>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
